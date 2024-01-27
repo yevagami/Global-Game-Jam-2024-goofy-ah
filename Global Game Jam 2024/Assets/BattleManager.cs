8005456fe3f9e3cb302395dbdd4e0443098df05a
yevagami <antonius.adriel@gmail.com>
2024-01-27 00:37:56 -0500

    Pushed a bunch of stuff

---
+++ Global Game Jam 2024/Assets/BattleManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour {
+    int currentTurnIndex = 0;
+    List<Character> participants = new List<Character>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
