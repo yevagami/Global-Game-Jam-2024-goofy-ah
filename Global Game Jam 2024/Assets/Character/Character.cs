761883232c7641f832e8f30650d9429fb0955a7c
Delianomy <delianomy@gmail.com>
2024-01-27 00:19:35 -0500

    Character Script

---
+++ Global Game Jam 2024/Assets/Character/Character.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    //public for now (maybe change later?)
+    public float health;
+    public float defense;
+    public float attackDMG;
+    //array for debuffs
+
+    bool isSad, isDepressed, isTaunting, isTaunted, isDoubting; 
+
+    // Update is called once per frame
+
+
+    //passive, attack, ultimate
+
+    //actions: attack, heal, 
+
+    void Update()
+    {
+        
+    }
+}
