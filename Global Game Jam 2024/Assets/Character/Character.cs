86efefe4fe5d256d5b0df85ea2fa7c3637727b46
worflor <126105721+worflor@users.noreply.github.com>
2024-01-27 09:05:45 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Global Game Jam 2024/Assets/Character/Character.cs
+++ Global Game Jam 2024/Assets/Character/Character.cs
@@ -4,41 +4,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public abstract class Character : MonoBehaviour
-{
+public abstract class Character : MonoBehaviour {
     //public for now (maybe change later?)
     public float health = 100.0f;
-    public float defense= 20.0f;
-    public float attackDMG= 15.0f;
+    public float defense = 20.0f;
+
+    public float attackDMG = 15.0f;
+
     //array for debuffs
     public bool isSad, isDepressed, isTaunting, isTaunted, isDoubting;
     public bool canFollowUp;
+
     public bool isBuffed, isAltered;
     //passive, attack, ultimate
-    
-    
     //actions: attack, heal, 
+
+
+    //  sound effects
+    protected Dictionary<string, AudioClip> characterSoundEffects;
+    protected AudioSource audioSource;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+
+        characterSoundEffects = new();
+        InitiateSoundEffects();
+    }
+    //  initiate all character sfx to the character sfx dictionary
+    protected abstract void InitiateSoundEffects();
+
+    //  play the sound effect at the sound label
+    protected void PlaySound(string soundLabel) {
+        if (characterSoundEffects.ContainsKey(soundLabel)) {
+            audioSource.clip = characterSoundEffects[soundLabel];
+            audioSource.Play();
+        } else {
+          Debug.LogWarning("Sound label '{soundLabel}' not in dictionary");  
+        }
+    }
+    
     abstract public bool StartTurn(int currentSkillPointCount);
-    public void TakeDamage(float recievedDamage) {
+
+    public void TakeDamage(float recievedDamage)
+    {
         float damageTaken = 0;
         damageTaken = recievedDamage * (1 - (defense / 100.0f));
         health -= damageTaken;
     }
-    abstract public void Update();
 
+    abstract public void Update();
 }
 
-public class CharacterAdriel : Character {
-    public override bool StartTurn(int currentSkillPointCount) {
+public class CharacterAdriel : Character
+{
+    protected override void InitiateSoundEffects()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool StartTurn(int currentSkillPointCount)
+    {
         if (currentSkillPointCount != 0) return true;
-        
+
         Debug.Log("NO SKILLPOINTS! you whore.");
         return false;
-
     }
+
     public override void Update()
     {
-
-
     }
 }
